c629559585ac674775f18c95de15edf007a158c8
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2025-03-26 21:27:40 -0300

    rr

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+    "icons": [
+      {
+        "src": "/icon-192.png", // 🟢 Altere este caminho para o ícone de 192x192
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/icon-512.png", // 🟢 Altere este caminho para o ícone de 512x512
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }
+  
